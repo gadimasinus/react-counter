901442c73626cb92550b0e1126f202f3be8f8529
gadimasinus <gadimasinus@yahoo.com>
2021-10-11 21:26:49 -0400

    initial commit
    initial commit

---
+++ src/Counter.js
@@ -0,0 +1,16 @@
+import react, {useState} from 'react';
+
+function Counter () {
+
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <button onClick = {()=> setCount(count+1)}>{count}</button>
+        </div>
+    );
+
+    
+}
+
+export default Counter;
